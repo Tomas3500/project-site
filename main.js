ca84fb432e66703ee19d1a3b2475a5556a0c3fec
Tomas3500 <smirnov9323@gmail.com>
2020-11-03 20:33:20 +0200

    added . gitignore

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-let blockBar = document.querySelectorAll(".myBar");
-let divArray = Array.from(blockBar);
-let out = document.getElementsByTagName('span');
-
-function progressBar(time) {
-    let start = 0;
-    let myBarIn = setInterval(() => {
-        for (let i = 0; i < divArray.length; i++) {
-            if (start > 100) {
-                clearInterval(myBarIn);
-            }
-            else {
-
-                divArray[i].style.width = start + '%';
-            }
-        }
-        out[0].innerHTML =  start + '%';
-        start++;
-    }, time);
-}
-
-progressBar(10);
+// let blockBar = document.querySelectorAll(".myBar");
+// let divArray = Array.from(blockBar);
+// let out = document.getElementsByTagName('span');
+//
+// function progressBar(time) {
+//     let start = 0;
+//     let myBarIn = setInterval(() => {
+//         for (let i = 0; i < divArray.length; i++) {
+//             if (start > 50) {
+//                 clearInterval(myBarIn);
+//             } else {
+//                 divArray[i].style.width = start + '%';
+//                 out[i].innerHTML = start + '%';
+//             }
+//         }
+//         start++;
+//     }, time);
+// }
+//
+// progressBar(50);
