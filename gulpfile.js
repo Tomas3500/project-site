32720e1d6015887a387c778bbb3d3fec1a2ab792
Tomas3500 <smirnov9323@gmail.com>
2020-11-15 21:27:47 +0200

    sect/education

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,76 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
+const {
+    src,
+    dest,
+    parallel,
+    series,
+    watch
+} = require('gulp');
+
+const browsersync = require("browser-sync").create();
+const cssnano = require('gulp-cssnano');
+const changed = require("gulp-changed");
+const imagemin = require("gulp-imagemin");
+const clean = require("gulp-clean");
+const autoprefixer = require('gulp-autoprefixer');
+const concatCss = require('gulp-concat-css');
+
+//cssnano
+function css() {
+    const source = './style/css.css';
+    return src(source)
+        .pipe(cssnano())
+        .pipe(dest('./build/css'))
+        .pipe(browsersync.stream());
 }
 
-exports.default = defaultTask;
+//autoprefixer
+// function ConcatCss() {
+//     const source = './style/**/*.css';
+//     return src(source)
+//         .pipe(autoprefixer({
+//             overrideBrowserslist: ['last 2 versions'],
+//             cascade: false
+//         }))
+//
+//         .pipe(concatCss('styles/bundle.css'))
+//         .pipe(dest('build'))
+// }
+
+//imagemin
+function img() {
+    const source = './img/*';
+    return src(source)
+        .pipe(imagemin())
+        .pipe(dest('build/images'))
+}
+
+//changed
+function html() {
+    return src('./*.html')
+        .pipe(dest('./build'))
+        .pipe(browsersync.stream());
+}
+
+function clear() {
+    return src('build', {read: false})
+        .pipe(clean());
+}
+
+function watchFiles() {
+    watch('./style/*', css);
+    watch('./*html', html);
+    watch('./img/*', img);
+    // watch('./style/*.scss', ConcatCss);
+}
+
+function browS() {
+    browsersync.init({
+        server: {
+            baseDir: "./"
+        },
+    });
+
+}
+
+exports.watch = parallel(watchFiles, browS);
+exports.default = series(clear, parallel(css, html, img));
