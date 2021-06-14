f30e66a78e2b49e2bcc524a509d9a55e0774e64b
unknown <smirnov9323@gmail.com>
2021-06-14 17:48:42 +0300

    fix js faile

--- main.js
+++ main.js
@@ -1,43 +1,23 @@
-let allDivBar = document.querySelectorAll('.myBar');
-let outResult = document.querySelectorAll('.out');
-let dataAttr;
-let out = []
-let res = [];
+let allDivBar = document.getElementsByClassName('myBar');
 
-function getAtt() {
+function showProgresBar(time) {
     for (let i = 0; i < allDivBar.length; i++) {
-        dataAttr = allDivBar[i].dataset.percent
-        out.push(allDivBar[i].dataset.percent)
-    }
-    res = out.map(i => {
-        return +i
-    })
-    return res
-
-}
-
-let timeInterval = setInterval(showProgresBar, 100)
-let start = 0;
-function showProgresBar() {
-    for (let i = 0; i < res.length; i++) {
-        if (start > res[i]) {
-            console.log(res[i])
-            clearInterval(timeInterval)
-
-        }
-        else {
-            start++
-            for (let i = 0; i < allDivBar.length; i++) {
-                allDivBar[i].style.width = start + '%'
+        let width = 0;
+        let dataA = allDivBar[i].getAttribute("data-percent")
+        let show = setInterval(() => {
+            if (width >= dataA) {
+                clearInterval(show)
             }
-            // console.log(start)
-        }
+            else {
+                width++
+                allDivBar[i].style.width = width + "%"
+                allDivBar[i].innerHTML = width
+            }
 
+        }, time)
     }
 
 }
-getAtt()
-
 
 
 
@@ -56,13 +36,9 @@ function myContatnInfo() {
     document.querySelector('.list-info ').innerHTML += `<li>Age:${contact.age} years</li>`;
     document.querySelector('.list-info').innerHTML += `<li>City:${contact.city}</li>`;
     document.querySelector('.contact').innerHTML += `<li>Phone:<a href="tel:+380999233489">+${contact.phone}</a></li>`;
-    document.querySelector('.color-gmail').innerHTML = `<li>Email:<a href="mailto:smirnov9323@gmail.com">+${contact.email}</a></li>`;
+    document.querySelector('.color-gmail').innerHTML += `<li>Email:<a href="mailto:smirnov9323@gmail.com">+${contact.email}</a></li>`;
     document.querySelector('.my-photo').src = contact.photo;
 
 }
-
-
-// progressBar()/
-// getAtt();
-showProgresBar()
+showProgresBar(50);
 myContatnInfo();
